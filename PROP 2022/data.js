8d72f5c329e350bb4f7809f2144a758b95af6141
gvsairoshan <74449502+gvsairoshan@users.noreply.github.com>
2022-06-16 15:24:39 +0530

    Add files via upload

---
+++ PROP 2022/data.js
@@ -0,0 +1,60 @@
+const data = {
+  coordinators: [
+    {
+      name: 'Dr.Devaraju.B.M',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Devaraju-B-M.pdf',
+      post: 'Asst. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Mr.-Devaraju-B-M.jpg',
+      guide: '(8th sem students)',
+    },
+    {
+      name: 'Dr. Shashidhar H R',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Dr.-H-R-Shashdhara.pdf',
+      post: 'Assoc. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Dr.-Shashidhara-H-R.jpg',
+      guide: '(M. Tech Students)',
+    },
+    {
+      name: 'Mr. Chetan.D.S',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/CHETAN-D-S.pdf',
+      post: 'Asst. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Dr.-M-J-Sudhamani.jpg',
+      guide: '(6th semester)',
+    },
+    {
+      name: 'Dr. Sudhamanu.M.J.',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Dr.Sudhamani-M-J.pdf',
+      post: 'Assoc. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Mr.-Chetan-D-S.jpg',
+      guide: '(4th Semester)',
+    },
+    {
+      name: 'Ms. Vidya.Y.',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/VIDYA-Y.pdf',
+      post: 'Asst. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Mrs.-Vidya-Y.jpg',
+      guide: '(2nd Semester)',
+    },
+    {
+      name: 'Ms. Chetana H R',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Chethana-H-R.pdf',
+      post: 'Asst. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Mrs.-Chrthana-H-R.jpg',
+      guide: '',
+    },
+    {
+      name: 'Mr. Srinivas V Biradar',
+      link: 'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Shrinivas-Biradar.pdf',
+      post: 'Asst. Prof.',
+      image:
+        'https://www.rnsit.ac.in/wp-content/uploads/2021/12/Mr.-Shrinivas-B.jpg',
+      guide: '',
+    },
+  ],
+};
